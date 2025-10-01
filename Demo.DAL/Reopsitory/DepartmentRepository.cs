958e4d3f6ef7beef25a67630a6d29de5c26143a0
abdallah-amin <abdoamen040466@gmail.com>
2025-10-02 00:09:23 +0300

    Department Repository

--- Demo.DAL/Reopsitory/DepartmentRepository.cs
+++ Demo.DAL/Reopsitory/DepartmentRepository.cs
@@ -1,5 +1,36 @@
-﻿namespace Demo.DAL.Reopsitory;
-public class DepartmentRepository
+﻿namespace Demo.DAL.Repository;
+public class DepartmentRepository(CompanyDbContext dbContext) : IDepartmentRepository
 {
+    private readonly DbSet<Department> _departments = dbContext.Departments;
+    public int Add(Department department)
+    {
+        _departments.Add(department);
+        return dbContext.SaveChanges();
+    }
 
+    public int Delete(Department department)
+    {
+        _departments.Remove(department);
+        return dbContext.SaveChanges();
+    }
+
+    public IEnumerable<Department> GetAll(bool trackChanges = false)
+    {
+        return trackChanges ?
+        _departments
+        .Where(d => d.IsDeleted == false).ToList() :
+        _departments.AsNoTracking()
+        .Where(d => d.IsDeleted == false).ToList();
+    }
+
+    public Department? GetById(int id)
+    {
+        return _departments.Find(id);
+    }
+
+    public int Update(Department department)
+    {
+        _departments.Update(department);
+        return dbContext.SaveChanges();
+    }
 }
--- Demo.DAL/Reopsitory/IDepartmentRepository.cs
+++ Demo.DAL/Reopsitory/IDepartmentRepository.cs
@@ -1,4 +1,9 @@
-﻿namespace Demo.DAL.Reopsitory;
+﻿namespace Demo.DAL.Repository;
 public interface IDepartmentRepository
 {
+    IEnumerable<Department> GetAll(bool trackChanges = false);
+    Department? GetById(int Id);
+    int Add(Department department);
+    int Update(Department department);
+    int Delete(Department department);
 }
