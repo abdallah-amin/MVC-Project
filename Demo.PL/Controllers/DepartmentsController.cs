91fe8889e08fb5e3c2a36f08e6abf0d1c598fc95
abdallah-amin <abdoamen040466@gmail.com>
2025-10-02 00:14:15 +0300

    Index DepartmentController

---
+++ Demo.PL/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using Demo.BLL.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.PL.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private IDepartmentServices _DepartmentServices;
+        public DepartmentsController(IDepartmentServices DepartmentServices)
+        {
+            _DepartmentServices = DepartmentServices;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
