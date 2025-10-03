3a00f34a83688c4f63eaffe4c550545e2624cc49
abdallah-amin <abdoamen040466@gmail.com>
2025-10-03 15:54:22 +0300

    create Page

--- Demo.PL/Controllers/DepartmentsController.cs
+++ Demo.PL/Controllers/DepartmentsController.cs
@@ -1,9 +1,12 @@
-﻿using Demo.BLL.Services;
+﻿using Demo.BLL.DataTransferObjects;
+using Demo.BLL.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.PL.Controllers
 {
-    public class DepartmentsController(IDepartmentServices departmentServices) : Controller
+    public class DepartmentsController(IDepartmentServices departmentServices,
+        ILogger<DepartmentsController> logger,
+        IWebHostEnvironment env) : Controller
     {
         [HttpGet]
         public IActionResult Index()
@@ -11,5 +14,35 @@ namespace Demo.PL.Controllers
             var department = departmentServices.GetAll();
             return View(department);
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(DepartmentRequest request)
+        {
+            // validation => Server side validation
+            if (!ModelState.IsValid)
+                return View(request);
+
+            try
+            {
+                var result = departmentServices.Add(request);
+                if (result > 0)
+                    return RedirectToAction(nameof(Index));
+                else
+                    ModelState.AddModelError(string.Empty, "Sorry you can't Add Department Now");
+            }
+            catch (Exception ex)
+            {
+                if (env.IsDevelopment())
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                else
+                    logger.LogError(ex, ex.Message);
+            }
+            return View(request);
+        }
+
     }
 }
