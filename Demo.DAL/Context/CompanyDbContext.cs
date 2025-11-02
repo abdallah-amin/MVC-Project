cc47ee2bd5a0e6563bd9f0c577333ef7e03934bc
abdallah-amin <abdoamen040466@gmail.com>
2025-11-02 22:46:31 +0200

    Add security entity with IdentityDbContext

--- Demo.DAL/Context/CompanyDbContext.cs
+++ Demo.DAL/Context/CompanyDbContext.cs
@@ -1,13 +1,16 @@
-﻿using System.Reflection;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Demo.DAL.Context;
-public class CompanyDbContext(DbContextOptions<CompanyDbContext> options) : DbContext(options)
+public class CompanyDbContext(DbContextOptions<CompanyDbContext> options)
+    : IdentityDbContext<ApplicationUser>(options)
 {
     public DbSet<Department> Departments { get; set; }
     public DbSet<Employee> Employees { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 
---
+++ Demo.DAL/Context/Configurations/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace Demo.DAL.Context.Configurations;
+internal class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(u => u.FirstName)
+            .HasColumnType("VarChar")
+            .HasMaxLength(250);
+
+        builder.Property(u => u.LastName)
+            .HasColumnType("VarChar")
+            .HasMaxLength(250);
+
+    }
+
+}
